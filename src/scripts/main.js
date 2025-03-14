1806841ae951651ad548ef9204746cc6f08f30f2
Gabrielagomesf <goomes.016@gmail.com>
2025-03-14 15:23:37 -0300

    atualiz4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,12 @@
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 
+// Toggle menu visibility on hamburger click
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('show');
 });
 
-function copyPix() {
-    const copyText = document.querySelector('.pix-info p').textContent.split(": ")[1];
-    navigator.clipboard.writeText(copyText).then(() => {
-        const message = document.getElementById('copy-message');
-        message.style.display = 'block';
-        setTimeout(() => message.style.display = 'none', 2000);
-    }).catch(err => {
-        console.error('Erro ao copiar: ', err);
-    });
-}
-
+// Fechar o menu após clicar em um item, quando estiver no mobile
 const menuItems = document.querySelectorAll('.menu li a');
 menuItems.forEach(item => {
     item.addEventListener('click', () => {
@@ -25,6 +16,7 @@ menuItems.forEach(item => {
     });
 });
 
+// Adicionar rolagem suave aos links de menu
 const menuLinks = document.querySelectorAll('a[href^="#"]');
 menuLinks.forEach(link => {
     link.addEventListener('click', (e) => {
@@ -41,8 +33,21 @@ menuLinks.forEach(link => {
     });
 });
 
+// Função para copiar o PIX
+function copyPix() {
+    const copyText = document.querySelector('.pix-info p').textContent.split(": ")[1];
+    navigator.clipboard.writeText(copyText).then(() => {
+        const message = document.getElementById('copy-message');
+        message.style.display = 'block';
+        setTimeout(() => message.style.display = 'none', 2000);
+    }).catch(err => {
+        console.error('Erro ao copiar: ', err);
+    });
+}
+
+// Fechar o menu quando a tela for redimensionada e maior que 768px
 window.addEventListener('resize', () => {
     if (window.innerWidth > 768) {
-        menu.classList.remove('show');
+        menu.classList.remove('show'); // Remove a classe show quando a tela for maior que 768px
     }
 });
